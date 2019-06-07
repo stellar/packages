98132f843ecb5d137a0bfe790551846409ad098b
Jacek Nykis <jacek@stellar.org>
2019-06-07 16:49:39 +0100

    Add build info labels to the stellar-core exporter

--- stellar-core-prometheus-exporter/stellar-core-prometheus-exporter.py
+++ stellar-core-prometheus-exporter/stellar-core-prometheus-exporter.py
@@ -8,12 +8,15 @@ import time
 
 # Prometheus client library
 from prometheus_client import start_http_server
-from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, SummaryMetricFamily, REGISTRY
+from prometheus_client.core import GaugeMetricFamily, SummaryMetricFamily, REGISTRY
 
 parser = argparse.ArgumentParser(description='simple stellar-core Prometheus exporter/scraper')
 parser.add_argument('--uri', type=str,
                     help='core metrics uri, default: http://127.0.0.1:11626/metrics',
                     default='http://127.0.0.1:11626/metrics')
+parser.add_argument('--info-uri', type=str,
+                    help='info endpoint uri, default: http://127.0.0.1:11626/info',
+                    default='http://127.0.0.1:11626/info')
 parser.add_argument('--port', type=int,
                     help='HTTP bind port, default: 9473',
                     default=9473)
@@ -33,7 +36,36 @@ def duration_to_seconds(duration, duration_unit):
   return eval(time_units_to_seconds[duration_unit])
 
 class StellarCoreCollector(object):
+  def __init__(self):
+    # Example: "stellar-core 11.1.0-unstablerc2 (324c1bd61b0e9bada63e0d696d799421b00a7950)"
+    self.build_regex = re.compile('stellar-core +(\d+)\.(\d+)\.(\d+)-([^ ]+) .*$')
+
+  def get_labels(self):
+    try:
+      response = requests.get(args.info_uri)
+      json = response.json()
+      build = json['info']['build']
+    except:
+      return {}
+    match = self.build_regex.match(build)
+    if not match:
+      return {}
+
+    labels = {
+      "ver_major": match.group(1),
+      "ver_minor": match.group(2),
+      "ver_patch": match.group(3),
+      "ver_extra": match.group(4),
+    }
+    return labels
+
   def collect(self):
+    # TODO handle missing labels, probably return 500?
+    labels = self.get_labels()
+    labels_p75 = labels.copy()  # Work on copy of labels variable to avoid other metrics getting quantile label
+    labels_p75.update({'quantile':'0.75'})
+    labels_p99 = labels.copy()  # Work on copy of labels variable to avoid other metrics getting quantile label
+    labels_p99.update({'quantile':'0.99'})
     response = requests.get(args.uri)
     json = response.json()
 
@@ -53,17 +85,19 @@ class StellarCoreCollector(object):
         else:
           # compute sum value
           total_duration = (metrics[k]['mean'] * metrics[k]['count'])
-        summary = SummaryMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], count_value=metrics[k]['count'], sum_value=(duration_to_seconds(total_duration, metrics[k]['duration_unit'])))
+        summary = SummaryMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], labels=labels.keys() )
+        summary.add_metric(labels.values(), count_value=metrics[k]['count'], sum_value=(duration_to_seconds(total_duration, metrics[k]['duration_unit'])))
         # add stellar-core calculated quantiles to our summary
-        summary.add_sample(underscores, labels={'quantile':'0.75'}, value=(duration_to_seconds(metrics[k]['75%'], metrics[k]['duration_unit'])))
-        summary.add_sample(underscores, labels={'quantile':'0.99'}, value=(duration_to_seconds(metrics[k]['99%'], metrics[k]['duration_unit'])))
+        summary.add_sample(underscores, labels=labels_p75, value=(duration_to_seconds(metrics[k]['75%'], metrics[k]['duration_unit'])))
+        summary.add_sample(underscores, labels=labels_p99, value=(duration_to_seconds(metrics[k]['99%'], metrics[k]['duration_unit'])))
         yield summary
-      elif metrics[k]['type'] == 'counter':
-        # we have a counter, this is a Prometheus Gauge
-        yield GaugeMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], value=metrics[k]['count'])
-      elif metrics[k]['type'] == 'meter':
-        # we have a meter, this is a Prometheus Counter
-        yield CounterMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], value=metrics[k]['count'])
+      elif metrics[k]['type'] == 'counter' or metrics[k]['type'] == 'meter':
+        # Meter is a Prometheus Counter but we only read and expose existing values.
+        # Prometheus Counter metric can only be incremented so we use Gauge instead
+        # and just set it to value returned by stellar-core
+        m = GaugeMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], labels=labels.keys())
+        m.add_metric(labels.values(), metrics[k]['count'])
+        yield m
 
 if __name__ == "__main__":
   REGISTRY.register(StellarCoreCollector())
