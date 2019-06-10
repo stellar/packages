beb747a28b5e03a365450b283883482ed559f2fe
Jacek Nykis <jacek@stellar.org>
2019-06-10 11:07:30 +0100

    stellar-core-prometheus-exporter: add info endpoint metrics

--- stellar-core-prometheus-exporter/stellar-core-prometheus-exporter.py
+++ stellar-core-prometheus-exporter/stellar-core-prometheus-exporter.py
@@ -4,6 +4,7 @@ import argparse
 import requests
 import re
 import time
+from datetime import datetime
 
 # Prometheus client library
 from prometheus_client import start_http_server
@@ -36,6 +37,11 @@ def duration_to_seconds(duration, duration_unit):
 
 class StellarCoreCollector(object):
   def __init__(self):
+    self.info_keys = ['ledger', 'peers', 'protocol_version', 'quorum', 'startedOn', 'state']
+    self.ledger_metrics = {'age': 'age', 'baseFee': 'base_fee', 'baseReserve': 'base_reserve',
+                           'closeTime': 'close_time', 'maxTxSetSize': 'max_tx_set_size',
+                           'num': 'num', 'version': 'version'}
+    self.quorum_metrics = ['agree', 'delayed', 'disagree', 'fail_at', 'missing']
     # Example: "stellar-core 11.1.0-unstablerc2 (324c1bd61b0e9bada63e0d696d799421b00a7950)"
     self.build_regex = re.compile('stellar-core +(\d+)\.(\d+)\.(\d+)-([^ ]+) .*$')
 
@@ -65,11 +71,9 @@ class StellarCoreCollector(object):
     labels_p75.update({'quantile':'0.75'})
     labels_p99 = labels.copy()  # Work on copy of labels variable to avoid other metrics getting quantile label
     labels_p99.update({'quantile':'0.99'})
+
     response = requests.get(args.uri)
-    json = response.json()
-
-    metrics   = json['metrics']
-
+    metrics = response.json()['metrics']
     # iterate over all metrics
     for k in metrics:
       underscores = re.sub('\.|-|\s', '_', k).lower()
@@ -100,6 +104,56 @@ class StellarCoreCollector(object):
         c = CounterMetricFamily(underscores, 'libmedida metric type: ' + metrics[k]['type'], labels=labels.keys())
         c.add_metric(labels.values(), metrics[k]['count'])
         yield c
+
+    # Export metrics from the info endpoint
+    response = requests.get(args.info_uri)
+    info = response.json()['info']
+    if not all([i in info for i in self.info_keys]):
+        print('WARNINNG: info endpoint did not return all required fields')
+        return
+
+    # Ledger metrics
+    for core_name, prom_name in self.ledger_metrics.items():
+      g = GaugeMetricFamily('ledger_{}'.format(prom_name),
+                            'Stellar core ledger metric name: {}'.format(core_name),
+                             labels=labels.keys())
+      g.add_metric(labels.values(), info['ledger'][core_name])
+      yield g
+
+    # Quorum metrics are reported under dynamic name for example:
+    # "quorum" : {
+    #   "758110" : {
+    #     "agree" : 3,
+    tmp = info['quorum'].values()[0]
+    for metric in self.quorum_metrics:
+      g = GaugeMetricFamily('quorum_{}'.format(metric), 'Stellar core quorum metric: {}'.format(metric), labels=labels.keys())
+      g.add_metric(labels.values(), tmp[metric])
+      yield g
+
+    # Peers metrics
+    g = GaugeMetricFamily('peers_authenticated_count', 'Stellar core authenticated_count count', labels=labels.keys())
+    g.add_metric(labels.values(), info['peers']['authenticated_count'])
+    yield g
+    g = GaugeMetricFamily('peers_pending_count', 'Stellar core pending_count count', labels=labels.keys())
+    g.add_metric(labels.values(), info['peers']['pending_count'])
+    yield g
+
+    g = GaugeMetricFamily('protocol_version', 'Stellar core protocol_version', labels=labels.keys())
+    g.add_metric(labels.values(), info['protocol_version'])
+    yield g
+
+    g = GaugeMetricFamily('synced', 'Stellar core sync status', labels=labels.keys())
+    if info['state'] == 'Synced!':
+      g.add_metric(labels.values(), 1)
+    else:
+      g.add_metric(labels.values(), 0)
+    yield g
+
+    g = GaugeMetricFamily('started_on', 'Stellar core start time in epoch', labels=labels.keys())
+    date = datetime.strptime(info['startedOn'], "%Y-%m-%dT%H:%M:%SZ")
+    g.add_metric(labels.values(), int(date.strftime('%s')))
+    yield g
+
 
 if __name__ == "__main__":
   REGISTRY.register(StellarCoreCollector())
